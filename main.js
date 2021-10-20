1962b4ffa1e93f790d656b77852b5c0332ecb1ea
davegregg@gmail.com <davegregg@gmail.com>
2021-10-20 10:53:01 -0400

    demo code, br1

---
+++ main.js
@@ -0,0 +1,23 @@
+
+
+
+function appendPokemonElement (name, parentElement) {
+    const pokemonElement = document.createElement("article")
+    pokemonElement.classList.add("pokemon-element")
+    pokemonElement.dataset.pokemon = name
+    parentElement.append(pokemonElement)
+
+    return pokemonElement
+}
+
+function appendPokemonSprite (imgURL, parentElement) {
+    const sprite = document.createElement("img")
+    sprite.src = imgURL
+    parentElement.append(sprite)
+}
+
+function appendPokemonTitle (name, parentElement) {
+    const title = document.createElement("h3")
+    title.append(name)
+    parentElement.append(title)
+}
